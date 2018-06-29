40bbb7fbdfbaba88bd23d7ee3764f7a92d48a20b
champax <champagnac.laurent@gmail.com>
2018-06-29 11:20:03 +0200

    Remove gevent version lock Bump version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ p_name = "pysoltcp"
 p_author = "Laurent Champagnac"
 p_email = "champagnac.laurent@gmail.com"
 p_url = "https://knock.center"
-p_version = "1.1.0"
+p_version = "1.1.1"
 
 # Load
 req_list, dep_list = requirement_read("requirements.txt")
